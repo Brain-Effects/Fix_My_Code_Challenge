b26034518d3f5b47d45eba8a37bc1369ceaae447
Brain-Effects <braineffects@github.com>
2024-08-21 16:53:53 -0700

    Delete dnodeint

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -9,36 +9,36 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *current, *temp;
-	unsigned int i = 0;
+    dlistint_t *current, *temp;
+    unsigned int i = 0;
 
-	if (!head || !*head)
-		return (-1);
+    if (!head || !*head)
+        return (-1);
 
-	current = *head;
+    current = *head;
 
-	if (index == 0)
-	{
-		*head = current->next;
-		if (*head)
-			(*head)->prev = NULL;
-		free(current);
-		return (1);
-	}
+    if (index == 0)
+    {
+        *head = current->next;
+        if (*head)
+            (*head)->prev = NULL;
+        free(current);
+        return (1);
+    }
 
-	while (current && i < index)
-	{
-		current = current->next;
-		i++;
-	}
+    while (current && i < index)
+    {
+        current = current->next;
+        i++;
+    }
 
-	if (!current)
-		return (-1);
+    if (!current)
+        return (-1);
 
-	temp = current->prev;
-	temp->next = current->next;
-	if (current->next)
-		current->next->prev = temp;
-	free(current);
-	return (1);
+    temp = current->prev;
+    temp->next = current->next;
+    if (current->next)
+        current->next->prev = temp;
+    free(current);
+    return (1);
 }
